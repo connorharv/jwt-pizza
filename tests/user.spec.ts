eb4c2d3a83b3d7e1a0c5dd439388a05d9e4e9bbb
Connor Harvey <connorharvey803@gmail.com>
2026-09-14 23:14:35 -0600

    Added updateUser tests for password/email/admin. [skip ci]

--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -35,3 +35,72 @@ test('updateUser', async ({ page }) => {
 
     await expect(page.getByRole('main')).toContainText('pizza dinerx');
 });
+
+test('updateUser with password and email', async ({ page}) => {
+    const email = `user${Math.floor(Math.random() * 10000)}@jwt.com`;
+    await page.goto('/');
+    await page.getByRole('link', { name: 'Register' }).click();
+    await page.getByRole('textbox', { name: 'Full name' }).fill('pizza diner');
+    await page.getByRole('textbox', { name: 'Email address' }).fill(email);
+    await page.getByRole('textbox', { name: 'Password' }).fill('diner');
+    await page.getByRole('button', { name: 'Register' }).click();
+
+    await page.getByRole('link', { name: 'pd' }).click();
+
+    await expect(page.getByRole('main')).toContainText('pizza diner');
+
+    await page.getByRole('button', { name: 'Edit' }).click();
+    await expect(page.locator('h3')).toContainText('Edit user');
+
+    const newEmail = `user${Math.floor(Math.random() * 10000)}@jwt.com`;
+
+    await page.locator('input[type="email"]').click();
+    await page.locator('input[type="email"]').fill(newEmail);
+    await page.locator('#password').click();
+    await page.locator('#password').fill('new password');
+    await page.getByRole('button', { name: 'Update' }).click();
+
+    // Test persistence
+    await page.getByRole('link', { name: 'Logout' }).click();
+    await page.getByRole('link', { name: 'Login' }).click();
+
+    await page.getByRole('textbox', { name: 'Email address' }).fill(newEmail);
+    await page.getByRole('textbox', { name: 'Password' }).fill('new password');
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    await page.getByRole('link', { name: 'pd' }).click();
+
+    await expect(page.getByRole('main')).toContainText(newEmail);
+})
+
+test('updateUser with admin', async ({ page }) => {
+    await page.goto('/');
+    const newPassword = `user${Math.floor(Math.random() * 10000)}`;
+
+    await page.getByRole('link', { name: 'Login' }).click();
+    await page.getByRole('textbox', { name: 'Email address' }).fill('a@jwt.com');
+    await page.getByRole('textbox', { name: 'Password' }).fill('admin');
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    await page.getByRole('link', { name: 'A', exact: true }).click();
+
+    await page.getByRole('button', { name: 'Edit' }).click();
+    await page.locator('#password').fill(newPassword);
+    await page.getByRole('button', { name: 'Update' }).click();
+
+    // Test persistence
+    await page.getByRole('link', { name: 'Logout' }).click();
+    await page.getByRole('link', { name: 'Login' }).click();
+
+    await page.getByRole('textbox', { name: 'Email address' }).fill('a@jwt.com');
+    await page.getByRole('textbox', { name: 'Password' }).fill(newPassword);
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    await page.getByRole('link', { name: 'A', exact: true }).click();
+
+    // Revert changes
+    await page.getByRole('button', { name: 'Edit' }).click();
+    await page.locator('#password').fill('admin');
+    await page.getByRole('button', { name: 'Update' }).click();
+
+})
